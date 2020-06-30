3bc480bb594941ef11779136196ce26f48a7e6c0
impartial-genre-2648 <vgscholz@gmail.com>
2020-06-30 00:27:53 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,7 +6,11 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
-introduction(Victor)
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+  
   
 
 
